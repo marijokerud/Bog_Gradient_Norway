851bef7ef43d03bd4faa6812cd2c79d0165eb081
marijokerud <mari.jokerud@gmail.com>
2025-08-21 13:05:01 +0200

    load data, species across gradient and functional trait

---
+++ code/01_read_data.R
@@ -0,0 +1,3 @@
+#ARTSFREKVENS DATA
+data<-read_excel(path = "Data/species-richness-2018.xlsx", sheet = "Species", col_names = TRUE)
+plant.type<-read_excel(path = "Data/species-richness-2018.xlsx", sheet = "Functional", col_names = TRUE)
