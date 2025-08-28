8be00c95120d0fa314b6a7860485391854f55659
marijokerud <mari.jokerud@gmail.com>
2025-08-28 15:29:34 +0200

    first version

---
+++ code/02_environmental_ordination.R
@@ -0,0 +1,90 @@
+library(tidyverse)
+library(vegan)
+
+#DATA
+climate <- climate.data %>% 
+  select(site, Precipitation, Tetraterm, lat, long) %>% 
+  gather(key = Variable, value = Deposition, - site, - lat, - long)
+
+enviromental <- enviromental.data %>% 
+  select(-Grid, -Site) %>% 
+  bind_rows(climate) %>% 
+  pivot_wider(names_from = Variable, values_from = Deposition)
+
+enviromental.PCA <- enviromental %>% 
+  select(-site) %>% #, -lat, -long
+  as.data.frame()
+
+
+# MAKE PCA
+res <- rda(enviromental.PCA, scale = TRUE, center = TRUE)
+
+summary(res)
+scores(res, display = "sites")
+
+output <- bind_cols(enviromental, scores(res, display = "sites"))
+#output <- out %>% 
+  select(quadrat, site, Total.N, PC1, PC2) %>% 
+  left_join(plot.info, by = "quadrat")
+
+  
+sp.env <- scores(res, display = "species") |>
+    as.data.frame() |>
+    rownames_to_column(var = "variables")
+  
+
+####### MAKE SITE PCA FIGURE  
+# calculate centroids
+centroids <- output  %>% 
+  left_join(output  %>% 
+              group_by(site)  %>% 
+              summarise(centroid1 = mean(PC1), 
+                        centroid2 = mean(PC2)),
+            by = "site")
+  
+e_B <- eigenvals(res)/sum(eigenvals(res))
+  
+Site <- output %>%
+  ggplot(aes(x = PC1, y = PC2, colour = site)) +
+  geom_point(size = 2) +
+    coord_equal() +
+    labs(x = glue("PCA1 ({round(e_B[1] * 100, 1)}%)"),
+         y = glue("PCA2 ({round(e_B[2] * 100, 1)}%)"),
+         tag = "(a)") +
+    guides(colour=guide_legend(title="Site")) +
+    theme_bw() +
+    theme(aspect.ratio = 1,
+          plot.tag.position = c(0, 0.8),
+          plot.tag = element_text(vjust = -1.5, hjust = -0.5, size = 10))
+ 
+ 
+#### ARROWS
+important_env <- sp.env %>%
+  mutate(length = sqrt(PC1^2 + PC2^2)) #%>%
+  filter(length > 0.6)
+
+#  #geom_point(size = 2) +
+  
+arrow.env <- output %>% 
+  ggplot(aes(x = PC1, y = PC2)) +
+  geom_segment(data = sp.env %>%
+                 mutate(length = sqrt(PC1^2 + PC2^2)),
+               aes(x = 0, y = 0, xend = PC1, yend = PC2),
+               arrow = arrow(length = unit(0.2,"cm")),
+               alpha = 0.75) +
+  geom_text_repel(data = important_env, aes(label = variables)) +
+  geom_text_repel(aes(label = site), color = 'grey70') +
+  labs(x = "PC 1", y = "PC 2", tag = "(b)") +
+  theme_bw() +
+  theme(aspect.ratio = 1,
+        plot.tag.position = c(0, 0.8),
+        plot.tag = element_text(vjust = -1.5, hjust = -0.5, size = 10))
+
+  
+  
+  
+  
+  
+  
+  
+  
