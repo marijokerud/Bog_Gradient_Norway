123683ad057fc1a377a8b3eede86ebe8400e5956
marijokerud <mari.jokerud@gmail.com>
2025-09-11 11:35:03 +0200

    splitt code for #SPECIES RICHNESS and evenness

--- code/02_species_richness.R
+++ code/02_species_richness.R
@@ -4,6 +4,18 @@ library(tibble)
 library(vegan)
 library(openxlsx)
 
+#SPECIES RICHNESS 
+richness.func <- comm.long %>% 
+  bind_rows(comm.long %>%                                         
+              group_by(plot_id) %>% 
+              summarise(n = sum(n)) %>% 
+              mutate(funtype = "total_richness")) %>%                            #calculate total species richness
+  bind_rows(comm.long %>% 
+              filter(funtype %in% c("live", "spha", "moss")) %>% 
+              group_by(plot_id) %>% 
+              summarise(n = sum(n)) %>% 
+              mutate(funtype = "total_moss"))   %>% 
+  rename(no_species = n)
 
 #SHANNON INDEX
 
@@ -13,21 +25,16 @@ richness.shannon <- as.data.frame(richness.shannon) %>%
   rownames_to_column("plot_id")
 
 
-#SPECIES RICHNESS & #EVENNESS
-richness.func <- comm.long %>% 
-  bind_rows(comm.long %>%                                         
-              group_by(plot_id) %>% 
-              summarise(n = sum(n)) %>% 
-              mutate(funtype = "total_richness")) %>%                            #calculate total species richness
-  rename(no_species = n)
 
+
+#EVENNESS
 richness <- richness.func %>% 
-  pivot_wider(names_from = funtype, values_from = no_species) %>% 
-  select(plot_id, total_richness) %>% 
-  mutate(log_richness = log(total_richness)) %>% 
+  mutate(log_richness = log1p(no_species)) %>% 
   left_join(richness.shannon, by = "plot_id") %>% 
   mutate(evenness = shannon_diversity/log_richness) 
 
+#pivot_wider(names_from = funtype, values_from = no_species) %>% 
+#select(plot_id, total_richness, shrub, forb, gram, live, spha, moss, total_moss) %>% 
 
 #write.xlsx(richness, "output/richness.xlsx")
 
